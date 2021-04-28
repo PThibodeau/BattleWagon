dc7288cfa45527103e102d1aacd059f38817fe53
PThibodeau <Thibodpt@gmail.com>
2021-04-27 21:39:05 -0400

    Basic setup information

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
 import EventDetails from '../views/EventDetails.vue'
-import About from '../views/About.vue'
+// import About from '../views/About.vue'
 
 const routes = [
   {
@@ -14,12 +14,13 @@ const routes = [
     name: 'EventDetails',
     props: true,
     component: EventDetails
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About
   }
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   component: About
+  // }
 ]
 
 const router = createRouter({
